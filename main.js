9546998c2267ce309fd9f8784f2db31af1575bec
freddie-devops <51332673+freddie-devops@users.noreply.github.com>
2019-06-14 12:41:54 -0400

    Test commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("<center><p><font size="3" color="green">Welcome to FreddieMac Spinnaker POC by Pricing Devops Team<\p></font></center>\n <center>This is a simple Node Js Application</center> \n <center>we'll be using a very basic example: a Hello World server written with Node.</center>\n <center>This is a Successful Response...:)</center>\n");
+  response.end("<center><p><font size="3" color="green">Welcome to FreddieMac Spinnaker POC \n by Pricing Devops Team<\p></font></center>\n <center>This is a simple Node Js Application</center> \n <center>we'll be using a very basic example: a Hello World server written with Node.</center>\n <center>This is a Successful Response...:)</center>\n");
 });
 
 // listen on localhost:8000
